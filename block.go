43c0a148f61e73194d1121742dbea26a53bec5c3
Administrator <Michael-glx@163.com>
2022-12-21 20:17:43 +0800

    Implement uintToByte

--- block.go
+++ block.go
@@ -1,12 +1,20 @@
 package main
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"time"
+)
 
 // 创世语
 const genesisInfo = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"
 
 type Block struct {
+	Version       uint64 // 区块版本号
 	PrevBlockHash []byte // 前区块哈希
+	MerkleRoot    []byte // 先填写为空，后续v4的时候使用
+	TimeStamp     uint64 // 从1970.1.1至今的秒数
+	Difficulty    uint64 // 挖矿的难度值，v2时使用
+	Nonce         uint64 // 随机数，挖矿找的就是它
 	Hash          []byte // 当前区块哈希
 	Data          []byte // 数据，目前使用字节流，V4开始使用交易代替
 }
@@ -14,7 +22,12 @@ type Block struct {
 // 创建区块，对Block的每一个字段填充数据即可
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := Block{
+		Version:       00,
 		PrevBlockHash: prevBlockHash,
+		MerkleRoot:    []byte{},
+		TimeStamp:     uint64(time.Now().Unix()),
+		Difficulty:    10,
+		Nonce:         10,
 		Hash:          []byte{}, // 先填充为空，后续会填充数据
 		Data:          []byte(data),
 	}
@@ -27,7 +40,12 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 // 为了生成区块哈希，我们实现一个简单函数，来计算哈希值，没有随机数，没有难度值
 func (block *Block) setHash() {
 	var data []byte
+	data = append(data, uintToByte(block.Version)...)
 	data = append(data, block.PrevBlockHash...)
+	data = append(data, block.MerkleRoot...)
+	data = append(data, uintToByte(block.TimeStamp)...)
+	data = append(data, uintToByte(block.Difficulty)...)
+	data = append(data, uintToByte(block.Nonce)...)
 	data = append(data, block.Data...)
 
 	hash := sha256.Sum256(data)
